dacebfd3c1d102331ccffae68a960bb9c92ba242
TamNguyenhoai <tam.nguyenhoai@gmail.com>
2024-07-20 17:22:06 +0700

    Add files via upload

---
+++ 27.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code to handle dropdown interactions if needed
+    // Currently, CSS handles the dropdown visibility
+});
